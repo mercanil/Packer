7f50e0f42c731923862809a6a72c3b5902c89bc7
mercanil <mercanil@gmail.com>
2019-05-13 01:19:20 +0100

    removed unused method.

--- src/main/java/com/mobiquityinc/packer/model/Item.java
+++ src/main/java/com/mobiquityinc/packer/model/Item.java
@@ -6,19 +6,9 @@ import lombok.Data;
  * Model of possible items can be used for packing
  */
 @Data
-public class Item implements Comparable<Item> {
+public class Item {
   private final int index;
   private final double weight;
   private final double cost;
 
-    /**
-     * compare by id for sorting ascending depends on index values
-     *
-     * @param item
-     * @return
-     */
-    @Override
-    public int compareTo(Item item) {
-        return (this.index - item.getIndex());
-  }
 }
